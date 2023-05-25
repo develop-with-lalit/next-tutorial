9d7867a3541b5bddfc7e6deffbf3787007113f86
develop-with-lalit <develop.with.lalit@gmail.com>
2023-05-25 09:45:59 +0530

    data fetching basics

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,46 @@
+import fs from "fs/promises";
 import Link from "next/link";
+import path from "path";
 
-const HomePage = () => {
+function HomePage({ products }) {
   return (
-    <div>
-      <h1> the home page</h1>
-      <ul>
-        <li>
-          <Link
-            href={{
-              pathname: "/portfolio",
-              // query:something
-            }}
-          >
-            Portfolio
-          </Link>
-        </li>
-        <li>
-          <Link href="/clients">Clients</Link>
-        </li>
-      </ul>
-    </div>
+    <ul>
+      {products.map((product) => {
+        return (
+          <li key={product.id}>
+            <Link href={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        );
+      })}
+    </ul>
   );
-};
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/about",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      products: data.products,
+      //   revalidate: 100
+    },
+  };
+}
 
 export default HomePage;
---
+++ pages/last-sales.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+const LastSalesPage = () => {
+  useEffect(() => {
+    // fetch(your api route);
+    console.log("hello");
+  }, []);
+
+  return (
+    <div>
+      <h1>LastSalesPage</h1>
+    </div>
+  );
+};
+
+export default LastSalesPage;
